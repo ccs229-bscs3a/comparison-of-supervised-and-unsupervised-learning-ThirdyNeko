9917ce3419cbf48543638275a316b8af8b22bdc1
Thirdy Neko <99775885+ThirdyNeko@users.noreply.github.com>
2024-04-07 11:16:00 +0800

    Update Clustering_Task.py

--- Clustering_Task.py
+++ Clustering_Task.py
@@ -13,7 +13,7 @@ def app():
     West Visayas State University"""
     st.text(text)
 
-    st.image('mushroom.jpg', caption="The mushroom edibility Dataset""")
+    st.image('wine.jpg', caption="The wine recognition Dataset""")
 
     text = """Data App: Supervised vs Unsupervised Learning Performance
     \nThis data app allows users to compare the performance of supervised learning (KNN) and unsupervised 
